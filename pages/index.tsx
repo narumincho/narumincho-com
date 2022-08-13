50188723b6b5dd9bef31efb14c905877c52fba8b
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-13 11:50:11 +0900

    emotion/css を使う

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
 import Head from "next/head";
+import iconPng from "../public/icon.png";
 
-const notionUrl = new URL(
-  "https://narumincho.notion.site/22961d0ee2924074a22ce37f405b941a"
-);
 export const TopPage = (): React.ReactElement => {
   return (
     <>
       <Head>
         <title>ナルミンチョの創作記録は Notion に移動しました</title>
+        <link rel="icon" type="image/png" href={iconPng.src} />
       </Head>
-      <div>
-        ナルミンチョの創作記録は
-        <a href={notionUrl.toString()}>{notionUrl.toString()}</a>
-        に移動しました.
+      <div
+        css={{
+          display: "grid",
+          backgroundColor: "black",
+          color: "white",
+          placeContent: "center",
+        }}
+      >
+        <div>
+          ナルミンチョの創作記録は
+          <a href="https://narumincho.notion.site/22961d0ee2924074a22ce37f405b941a">
+            Notion
+          </a>
+          に移動しましたが. このサイトでもなにかするかも
+        </div>
       </div>
     </>
   );
